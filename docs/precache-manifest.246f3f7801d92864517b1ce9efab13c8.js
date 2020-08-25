5b430ae750cd15adfe3d0f89da949a284eb74c3c
Adrian Matos <adrianmatoschincha@gmail.com>
2020-08-25 17:11:13 -0500

    docs agregado

---
+++ docs/precache-manifest.246f3f7801d92864517b1ce9efab13c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e237ca94020ef3043ed28b9d600bc984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3fa45427c5808ca43d",
+    "url": "/static/css/main.c2eed9b0.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3fa45427c5808ca43d",
+    "url": "/static/js/main.a989f00f.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
